3c914eb0f5e91d4b89748a79fa7dbfc87e039435
Maja Blagic <majablgic505@gmail.com>
2021-08-23 00:37:16 +0200

    UserDAO

---
+++ Seoul-Food/src/dao/UserDAO.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.util.LinkedHashMap;
+
+import beans.User;
+
+public class UserDAO {
+
+	private LinkedHashMap<String, User> users;
+	
+	private String path; 
+	public UserDAO() {
+		//Constructor
+	}
+}
