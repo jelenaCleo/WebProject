470a726387247551d944829dd5eca03ca007fe1b
Maja Blagic <majablgic505@gmail.com>
2021-08-23 16:16:24 +0200

    Router problems

---
+++ Seoul-Food/WebContent/app/Jelena.js
@@ -0,0 +1,26 @@
+Vue.component("jelena",{
+    template: `
+    <div>
+   <h1>Jelena stelena</h1>
+    
+   <img src="img/jelena.jpg" alt="Stelena jelena">
+   <br>
+   <br>
+   <label><b>Username</b></label>
+   <input type="text" placeholder="Enter Username" required>
+   <br><br>
+   <label><b>Password</b></label>
+   <input type="password" placeholder="EnterPassword" required>
+
+   <button type="submit">Login</button>
+   <p>
+   <a href="#/x">gotox</a>
+</p>
+
+
+    </div>
+    
+    
+    `
+
+});
--- Seoul-Food/WebContent/app/app.js
+++ Seoul-Food/WebContent/app/app.js
@@ -1,10 +1,29 @@
+const Jelena = {template: '<jelena></jelena>'}
+const x1 = {template: '<x1></x1>'}
+
+
+
+const router = new VueRouter({
+
+    mode: 'hash',
+    routes:[
+        {path: '/', component : Jelena},
+        {path: '/x', component:x1}
+       
+    ]
+
+});
+
 
 
 var app = new Vue({
+    router,
     el:'#app',
     data:{
         test: 'test-1-2',
     }
+    
+    
 
 
 
