e0fb25b4ce511d159ec4b49364eae27eb55cef38
jelenaCleo <jelenastajic98@uns.ac.rs>
2021-08-30 02:04:54 +0200

    Odradjen edit profila bez menjanja datuma,datepisker ne radi

--- Seoul-Food/WebContent/app/administrators/components/adminProfil.js
+++ Seoul-Food/WebContent/app/administrators/components/adminProfil.js
@@ -1,9 +1,31 @@
+ function fixDate(user) {
+		
+			user.birthday = new Date(parseInt(user.birthday));
+			
+			return user;
+		}
+
+
 Vue.component("admin-profil", {
 	data() {
 		return {
 			user: {},
-			changedUser: {},
-			isHidden : true
+			editedUser: {
+				username: '',
+				name: '',
+				surname: '',
+				gender: '',
+				role: '',
+				password: '',
+				birthday: null
+			},
+			changedUser: {
+				username: '',
+				password: '',
+				newPassword: '',
+			},
+			isHidden: true,
+			message: ''
 		}
 	},
 
@@ -11,72 +33,138 @@ Vue.component("admin-profil", {
 <div>
     <h1> Zdravo {{user.userName}}! </h1>
     <section class="container mt-4">
-        <table class="table table-striped">
+        <table class="table table-striped"  style="width:60%;">
             <thead>
                 <tr>
-                    <th>Korisnicko ime</th>
-                    <th>Ime</th>
-                    <th>Prezime</th>
-                    <th>Pol</th>
-                    <th>Uloga</th>
-                    <th>Datum rodjenja</th>
+                    <th style="width:150px;">Korisnicko ime</th>
+                    <th style="width:150px;">Ime</th>
+                    <th style="width:150px;">Prezime</th>
+                    <th style="width:150px;">Pol</th>
+                    <th style="width:150px;">Uloga</th>
+                    <th style="width:150px;">Datum rodjenja</th>
                 </tr>
             </thead>
             <tbody>
-
-                <td> <input type="text" v-model="user.username" placeholder="Name"> </td>
-                <td> <input type="text" v-model="user.name" placeholder="Name"> </td>
-                <td> <input type="text" v-model="user.surname" placeholder="Name"> </td>
-                <td> <input type="text" v-model="user.gender" placeholder="Name"> </td>
-                <td> <input type="text" v-model="user.role" placeholder="Name" readonly> </td>
-                <td> <input type="text" v-model="user.birthday" placeholder="Name"> </td>
-                <td align="center">
-                    <button v-if="user.blocked == '1' && user.role != 'ADMIN' " type="button"
-                        @click="unblockUser(user)"><i class="fa fa-check" aria-hidden="true"></i> Unblock </button>
-                    <button v-if="user.blocked == '0' && user.role != 'ADMIN' " type="button"
-                        @click="blockUser(user)"><i class="fa fa-ban" aria-hidden="true"></i> Block </button>
-                </td>
+                <td> <input style="width:150px;" type="text" v-model="editedUser.username" placeholder="Name"> </td>
+                <td> <input style="width:150px;" type="text" v-model="editedUser.name" placeholder="Name"> </td>
+                <td> <input style="width:150px;" type="text" v-model="editedUser.surname" placeholder="Name"> </td>
+                <td> <input style="width:150px;" type="text" v-model="editedUser.gender" placeholder="Name"> </td>
+                <td> <input style="width:150px;" type="text" v-model="editedUser.role" placeholder="Name" readonly> </td>
+                <td> <vuejs-datepicker v-model="this.editedUser.birthday" format="dd-MM-yyyy" ></vuejs-datepicker> </td>
                 </tr>
-
             </tbody>
         </table>
-        
-        
-    </section>
-    <br><br>
-    <button @click="saveChanges()" class="saveChanges"><i class="fa fa-check" aria-hidden="true"></i> Save changes
-    </button>
-    <br><br>
-    <button @click="showPasswordBox()"><i class="fa fa-check" aria-hidden="true"></i> Promeni sifru
-    </button>
-    
-	<div id="passwordBox" class="toshow" style="visibility:hidden"  >
-        
-        <p>uuuuuuu</p>
-    </div>
-	
 
-	
-</div>	 
+
+    </section>
+    <section class="container mt-4">
+        <br><br>
+        <button @click="saveChanges()" ><i class="fa fa-check" aria-hidden="true"></i> Save changes
+        </button>
+        <button @click="showPasswordBox()"><i aria-hidden="true"></i> Promeni sifru
+        </button>
+
+        <div id="passwordBox" class="toshow" style="visibility:hidden">
+            <div style="width:400px;">
+                <br></br>
+                <div style="width:200px; float:left;">
+                    <label for="place" style="height:30px;">Trenutna lozinka: </label>
+                    <br></br>
+                    <label for="place" style="height:30px;">Nova lozinka: </label>
+                    <br></br>
+                    <label for="place" style="height:30px;">Potvrdite novu lozinku: </label>
+
+                </div>
+                <div style="width:200px; float:right;">
+                    <input type="password" v-model="changedUser.password" placeholder="password">
+                    <br></br>
+                    <input type="password" v-model="changedUser.newPassword" placeholder="new password">
+                    <br></br>
+                    <input id="passwordConfirm" type="password"  placeholder="new password">
+                </div>
+
+            </div>
+
+            <button @click="saveNewPassword()"><i aria-hidden="true"></i> Sacuvaj novu lozinku </button>
+        </div>
+    </section>
+	<p>{{this.message}}</p>
+
+</div>
     
     `
-    ,
-    methods: {
+	,
+	methods: {
 
 		showPasswordBox: function() {
-		if(this.isHidden){
-			document.getElementById("passwordBox").style.visibility = 'visible' ;
-			this.isHidden = false;
-			console.log("visible");
-			}else{
-			document.getElementById("passwordBox").style.visibility = 'hidden' ;
-			this.isHidden = true;
-			console.log("hidden");
+			if (this.isHidden) {
+				document.getElementById("passwordBox").style.visibility = 'visible';
+				this.isHidden = false;
+				console.log("visible");
+			} else {
+				document.getElementById("passwordBox").style.visibility = 'hidden';
+				this.isHidden = true;
+				console.log("hidden");
+				this.message = "";
+			}
+		},
+		saveNewPassword: function() {
+			if (this.user.password == this.changedUser.password) {
+				this.changedUser.username = this.user.username;
+				if (this.changedUser.newPassword == document.getElementById("passwordConfirm").value) {
+					axios
+						.post('rest/users/changePassword', this.changedUser)
+						.then(response => {
+							toastr["success"]("Success changes!!", "Success!");
+							this.message = "sifra promenjena";
+						})
+						.catch(err => {
+							toastr["error"]("Failed during changes :(", "Fail");
+						})
+				}
+			}
+		},
+		saveChanges: function() {
+			if (true) {
+					var u = {username:this.editedUser.username,name:this.editedUser.name,surname:this.editedUser.surname,role:this.editedUser.role,password:this.editedUser.password,gender:this.editedUser.gender,birthday:this.editedUser.birthday.getTime()}
+					console.log(this.editedUser.birthday + "editedUser birthday");
+					console.log(this.user.birthday + "user birthday");
+					axios
+						.post('rest/users/editUser', this.editedUser)
+						.then(response => {
+							toastr["success"]("Success changes!!", "Success!");
+							this.message = "user promenjen";
+						})
+						.catch(err => {
+							toastr["error"]("Failed during changes :(", "Fail");
+						})
+				
 			}
 		}
 	},
-	mounted() {
-		axios.get('rest/users/myProfile').then(response => (this.user = response.data));
 
+	mounted() {
+		axios.get('rest/users/myProfile').then(response => {this.user = response.data;
+		console.log(this.user.birthday);
+		this.editedUser.username = this.user.username;
+		this.editedUser.name = this.user.name;
+		this.editedUser.surname = this.user.surname;
+		this.editedUser.gender = this.user.gender;
+		this.editedUser.role = this.user.role;
+		this.editedUser.birthday = new Date(parseInt(this.user.birthday));
+		this.editedUser.password = this.user.password;
+		this.user= fixDate(this.user);
+		console.log(this.user.birthday);
+	});
+
+	},
+	components: {
+      	vuejsDatepicker
+    },
+	filters: {
+		dateFormat: function(value, format) {
+			var parsed = moment(value);
+			return parsed.format(format);
+		}
 	}
 });
--- Seoul-Food/src/dto/UserDTO.java
+++ Seoul-Food/src/dto/UserDTO.java
@@ -2,13 +2,16 @@ package dto;
 
 import java.util.Date;
 
+import javax.json.bind.annotation.JsonbDateFormat;
+
 public class UserDTO {
 	public String username;
 	public String password;
 	public String gender;
 	public String name;
 	public String surname;
-	public String birthday;
+	@JsonbDateFormat(JsonbDateFormat.TIME_IN_MILLIS)
+	public Date birthday;
 	public String role;
 
 }
